6faa85e7d520f0844f63241411662bd820805eb0
arturokumura <arturokumura2008@gmail.com>
2026-01-11 13:14:55 -0300

    criei ex03

---
+++ ex03/src/Main.java
@@ -0,0 +1,13 @@
+import.java.util.Scanner
+public class main {
+    public static void Main(String[] args) { // Ponto de entrada do programa}
+        Scanner sc= new Scanner (System+in)
+        int x;
+        int y;
+
+        x=sc.NextInt();
+        y=sc.NestInt();
+
+        System.out.println("Digite um numero")
+    }
+}
