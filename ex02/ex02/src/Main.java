b4b1fe81562c32104a3f4760c14f9334e5d2b493
arturokumura <arturokumura2008@gmail.com>
2026-01-10 21:03:25 -0300

    Ex- saida de dados

--- ex02/ex02/src/Main.java
+++ ex02/ex02/src/Main.java
@@ -1,5 +1,32 @@
- Public class main {
+
+
+public class Main {
   public static void main(String[] args) {
+
+
+        String product1 = "Computer";
+        String product2 = "Ofice deck";
+
+        int age = 30;
+        int code = 0366;
+        char gender= 'F';
+
+        double price1 = 2100.00;
+        double price2 = 560.00;
+        double measure= 44.37253698;
+
+        System.out.println ("Products:");
+        System.out.println(product1 + ", which price is $ " + price1);
+        System.out.println(product2 + ", which price is $ " + price2);
+        System.out.println();
+        System.out.println("Recorde: " + age +" years old " + ", code " + code + " and gender: " + gender);
+        System.out.println();
+        System.out.println("Measue with eight decimal places: " + measure );
+        System.out.print("Rounded (three decimal places): " );
+        System.out.printf("%.3f", measure);
+     
+
 
   }
 }
+
