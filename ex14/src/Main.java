07f68b60a55c76d633ad81af42c982c563b74557
arturokumura <arturokumura2008@gmail.com>
2026-01-12 13:45:44 -0300

    ex14- intervalos

---
+++ ex14/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+    double x= sc.nextDouble();
+
+    if (x < 0.0 || x > 100.0) {
+        System.out.println("Fora de intervalo");
+    }
+        else if (x <= 25.0) {
+            System.out.println("Intervalo [0,25]");
+        }
+        else if (x <= 50.0) {
+            System.out.println("Intervalo (25,50]");
+        }
+        else if (x <= 75.0) {
+            System.out.println("Intervalo (50,75]");
+        }
+        else {
+            System.out.println("Intervalo (75,100]");
+        }
+
+        sc.close();
+    }
+    }
+
