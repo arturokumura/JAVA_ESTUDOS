4ea3b4c56db279df4440c1147f0d1f32c1277498
arturokumura <arturokumura2008@gmail.com>
2026-01-13 18:09:10 -0300

    ex17- while

---
+++ ex17/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Locale;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+    int x= sc.nextInt();
+
+
+    while (x!=2002) {
+        System.out.println("SENHA INVÁLIDA");
+       x= sc.nextInt();
+    }
+        System.out.println("Acesso Permitido");
+
+        sc.close();
+    }
+}
