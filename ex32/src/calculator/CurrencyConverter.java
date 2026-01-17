2a88caeeae237c1379fa0bb2136a228f552d396b
arturokumura <arturokumura2008@gmail.com>
2026-01-17 15:35:04 -0300

    ex32- dollar

---
+++ ex32/src/calculator/CurrencyConverter.java
@@ -0,0 +1,10 @@
+package calculator;
+
+public class CurrencyConverter {
+    public  double DOLLAR = 3.10;
+    public double n;
+
+    public double Calculator() {
+        return n * DOLLAR + (n*DOLLAR)*0.06;
+    }
+}
