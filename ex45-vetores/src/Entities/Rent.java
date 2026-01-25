eeb95f670660ccf7dfed76e898601d481c5c4c05
Artur Okumura <arturokumura2008@gmail.com>
2026-01-25 20:35:53 -0300

    ex45- "pensionario"

---
+++ ex45-vetores/src/Entities/Rent.java
@@ -0,0 +1,37 @@
+package Entities;
+
+public class Rent {
+    private String name;
+    private String email;
+
+    public Rent() {
+
+    }
+
+    public Rent(String email, String name) {
+        this.email = email;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return  name +
+                ", " + email ;
+    }
+}
---
+++ ex45-vetores/src/application/Main.java
@@ -0,0 +1,43 @@
+package application;
+
+import Entities.Rent;
+import java.util.Locale;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("How many rooms will be rented? ");
+        int n = sc.nextInt();
+
+        Rent[]vect = new Rent[10];
+
+
+
+        for (int i=0; i<n; i++) {
+            System.out.print("Rent #" + i + ":");
+            System.out.print("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.print("Email: ");
+            String email = sc.next();
+            System.out.print("Room: ");
+            int roomNumber = sc.nextInt();
+            vect[roomNumber] = new Rent(name, email);
+        }
+
+
+
+        System.out.println();
+        System.out.println("Busy rooms:");
+        for (int i= 0; i < 10; i++){
+            if (vect[i] != null) {
+                System.out.println(i + ": " + vect[i] );
+            }
+
+        }
+
+        sc.close();
+    }
+}
