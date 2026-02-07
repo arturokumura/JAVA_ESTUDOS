49ea4b2c811512dcd5e101f4e90d213178577898
Artur Okumura <arturokumura2008@gmail.com>
2026-02-07 17:41:32 -0300

    Add files via upload

---
+++ ex58 -set/src/Main.java
@@ -0,0 +1,43 @@
+import entities.LogEntry;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Date;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter file full path: ");
+        String path = sc.nextLine();
+
+        try(BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+            Set<LogEntry> set = new HashSet<>();
+
+            String line = br.readLine();
+            while (line != null) {
+                String[] fields = line.split(" ");
+                String username = fields[0];
+                Date moment = (Date) Date.from(Instant.parse(fields[1]));
+
+                set.add(new LogEntry(username, moment));
+
+                line = br.readLine();
+            }
+
+            System.out.println("Total users: " + set.size());
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+    }
+}
